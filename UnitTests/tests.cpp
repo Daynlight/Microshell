2b15e2d70fa711495164df3c487f41cfb7f1a20e
Daynlight <danielstodulski000@gmail.com>
2025-11-25 19:28:26 +0100

    cleanup

--- UnitTests/tests.cpp
+++ UnitTests/tests.cpp
@@ -3,22 +3,10 @@
 
 #include "Assert.h"
 
-#include "vector.h"
-#include "unordered_map.h"
-#include "string.h"
-
-
 
 
 int main(){
   Tests::Assert assert;
-  Structures::UnitTests::Unordered_map unordered_map;
-  Structures::UnitTests::Vector vector;
-  Structures::UnitTests::String string;
-
-  assert.assertion("== Vector result ==\n", vector.runAll());
-  assert.assertion("== Unordered_map result ==\n", unordered_map.runAll());
-  assert.assertion("== String result ==", string.runAll());
 
   if(!assert.results("Unit Tests Result"))
     return -1;
