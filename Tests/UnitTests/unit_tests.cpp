a96445d086c1f205655c3097345381690eb749f7
Daynlight <danielstodulski000@gmail.com>
2025-11-26 13:31:19 +0100

    Vector

--- Tests/UnitTests/unit_tests.cpp
+++ Tests/UnitTests/unit_tests.cpp
@@ -3,13 +3,15 @@
 
 #include "Assert.h"
 
+#include "Vector/vector.h"
 
 
 int main(){
   Tests::Assert assert;
 
+  Structures::UnitTests::Vector vector_tests;
 
-
+  assert.assertion("Vector Tests", vector_tests.runAll());
 
 
   if(!assert.results("Unit Tests Result"))
